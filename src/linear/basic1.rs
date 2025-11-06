eec24401ec6b2677030d266323079790f418c784
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-11-06 21:07:22 +1100

    feat:update

--- src/linear/basic1.rs
+++ src/linear/basic1.rs
@@ -11,8 +11,11 @@ pub fn example() {
     let x_train = Tensor::<B, 2>::from_floats([[1.0_f32], [2.0], [3.0]], &device);
     let y_train = Tensor::<B, 2>::from_floats([[2.0_f32], [4.0], [6.0]], &device);
 
+    let w = Tensor::<B, 1>::zeros([1], &device).require_grad();
+    let b = Tensor::<B, 1>::zeros([1], &device).require_grad(); // shape: [1]
     println!("{}", x_train);
-    println!("{:?}",x_train.shape());
+    println!("{:?}", x_train.shape());
     println!("{}", y_train);
 
+    println!("{}",w);
 }
