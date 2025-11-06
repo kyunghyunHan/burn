80abaf487500cef8fd04495a8e0a4946904448ad
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-11-06 21:00:24 +1100

    feat:update

--- src/linear/basic1.rs
+++ src/linear/basic1.rs
@@ -8,7 +8,11 @@ pub fn example() {
 
     let device = WgpuDevice::default();
 
-    let _x_train = Tensor::<B, 2>::from_floats([[1.0_f32], [2.0], [3.0]], &device);
+    let x_train = Tensor::<B, 2>::from_floats([[1.0_f32], [2.0], [3.0]], &device);
+    let y_train = Tensor::<B, 2>::from_floats([[2.0_f32], [4.0], [6.0]], &device);
 
-    println!("{}", _x_train);
+    println!("{}", x_train);
+    println!("{:?}",x_train.shape());
+    println!("{}", y_train);
+
 }
