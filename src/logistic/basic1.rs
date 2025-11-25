b0c2e7ff1b15bec68232c253de2c34a0a98d926c
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-11-25 22:26:27 +1100

    feat:update

--- src/logistic/basic1.rs
+++ src/logistic/basic1.rs
@@ -1,5 +1,12 @@
 use burn::backend::autodiff;
-
+use burn::backend::wgpu::{Wgpu, WgpuDevice};
+use burn::data::dataloader::{batcher::Batcher, DataLoaderBuilder};
+use burn::data::dataset::Dataset;
+use burn::nn::{
+    loss::{MseLoss, Reduction},
+    Linear, LinearConfig, Lstm, LstmConfig,
+};
+use burn::tensor::Int; // 꼭 추가!
 
 pub fn example(){
     println!("logistic");
